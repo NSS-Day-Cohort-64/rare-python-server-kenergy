7027239b0227d62539717364cac794debb9fb92f
Lance Buckley <erchancy@gmail.com>
2023-07-25 11:55:01 -0500

    Added all models and import them into __init__

---
+++ models/comment.py
@@ -0,0 +1,8 @@
+class Comment():
+    """Creates the model for all comments"""
+
+    def __init__(self, id, author_id, post_id, content):
+        self.id = id
+        self.author_id = author_id
+        self.post_id = post_id
+        self.content = content
---
+++ models/tag.py
@@ -0,0 +1,6 @@
+class Tag():
+    """Creates the model for all tags"""
+
+    def __init__(self, id, label):
+        self.id = id
+        self.label = label
